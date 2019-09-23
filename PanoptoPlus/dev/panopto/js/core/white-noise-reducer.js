e2b75e044ef1b62e77c59b62294aa4dad4e45d3f
Matt Lee <crazoter@gmail.com>
2019-09-23 10:25:56 +0800

    Modify filter params

--- PanoptoPlus/dev/panopto/js/core/white-noise-reducer.js
+++ PanoptoPlus/dev/panopto/js/core/white-noise-reducer.js
@@ -1,13 +1,8 @@
 /**
-     * WhiteNoiseReducer reduces white noise by adding a bandpass filter and compression.
-     */
+ * WhiteNoiseReducer reduces white noise by adding a bandpass filter and compression.
+ */
 let WhiteNoiseReducer = (() => {
     class WhiteNoiseReducer {
-        /**
-         * Modifications to the noise https://stackoverflow.com/questions/16949768/how-can-i-reduce-the-noise-of-a-microphone-input-with-the-web-audio-api
-         * @param {DOM} mediaElem MediaElement (video) which will have its audio modified
-         * @returns Object containing the compressor and filter
-         */
         /**
          * Adding a bandpass filter to the sound to reduce white noise https://stackoverflow.com/questions/16949768/how-can-i-reduce-the-noise-of-a-microphone-input-with-the-web-audio-api
          * 
@@ -16,28 +11,20 @@ let WhiteNoiseReducer = (() => {
          * @returns returns new currentSource
          */
         static linkToAudioContextThenReturnTail(audioContext, currentSource) {
+            //See https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/createDynamicsCompressor
+            //See https://developer.mozilla.org/en-US/docs/Web/API/BiquadFilterNode
             let compressor = audioContext.createDynamicsCompressor(),
                 filter = audioContext.createBiquadFilter();
             //Setup Compressor
             compressor.threshold.value = -50;
             compressor.knee.value = 40;
             compressor.ratio.value = 12;
-            //https://github.com/GoogleChromeLabs/web-audio-samples/wiki/DynamicsCompressor.reduction
-            //Value is read-only so this section is commented out
-            /*
-            if (typeof compressor.reduction === 'number' 
-                || typeof compressor.reduction === 'float') {
-                compressor.reduction = -20;
-            } else {
-                compressor.reduction.value = -20;
-            }*/
             compressor.attack.value = 0;
             compressor.release.value = 0.25;
 
             //Setup Filter
-            filter.Q.value = 1.80;
-            filter.frequency.value = 355;
-            filter.gain.value = 25;
+            filter.Q.value = 4;
+            filter.frequency.value = 475;
             filter.type = 'bandpass';
             filter.connect(compressor);
 
